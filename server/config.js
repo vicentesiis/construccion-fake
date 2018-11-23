1816d0ecaca4ce56df418dbce0fe9ed7c30f2023
Vicente Cantú <vice.00@hotmail.com>
2018-11-23 12:28:49 -0600

    db

--- server/config.js
+++ server/config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const key        = 'thisisasecretkeythatshouldbemoresecret';
-// const mongoDBURI = 'mongodb://constructionDev:Construction123@ds139921.mlab.com:39921/construction-dev';
+const mongoDBURI = 'mongodb://constructionDev:Construction123@ds139921.mlab.com:39921/construction-dev';
 
-const mongoDBURI = 'mongodb://localhost:27017/construccion';
+// const mongoDBURI = 'mongodb://localhost:27017/construccion';
 
 module.exports = {
     jwtKey    : key,
